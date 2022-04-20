804f02b6ecd741cf7135c76ee4f37964663c89c2
lorenzo.biraghi <biraghilorenzo02@gmail.com>
2022-04-20 19:10:27 +0200

    Progetto base

---
+++ src/main/java/com/example/demo/EserciziApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EserciziApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EserciziApiRestApplication.class, args);
+	}
+
+}
